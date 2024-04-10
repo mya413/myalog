9e91431847cf9f2f668c2b2f4b2f4c427deb3c62
mya413 <lsm940413@gmail.com>
2024-04-10 22:10:04 +0900

    design: portfolio 페이지 탭 목업

---
+++ src/components/molecules/Card.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default function Card() {
+  return (
+    <CardStyle>
+      <h1>카드</h1>
+    </CardStyle>
+  );
+}
+
+const CardStyle = styled.div``;
---
+++ src/components/organisms/Experience.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export default function Experience() {
+  return <ExperienceStyle>경험탭</ExperienceStyle>;
+}
+
+const ExperienceStyle = styled.div``;
