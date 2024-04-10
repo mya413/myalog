54676c84aca32fc9fac436cc40e6845a3e4a1119
mya413 <lsm940413@gmail.com>
2024-04-10 12:30:55 +0900

    chore: baseUrl 및 alias path 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/myalog/",
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    port: 3000,
+  },
+  base: process.env.NODE_ENV === "development" ? "/" : "./",
 });
