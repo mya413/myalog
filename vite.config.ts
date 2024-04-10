b02b1c1cd731463c2209c2886511f7bd748d363c
이승미 <lsm940413@gmail.com>
2024-04-10 23:54:19 +0900

    build base url update

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  server: {
-    port: 3000,
-  },
-  base: "",
+  base: "/myalog/",
 });
