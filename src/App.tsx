04314685a65747fe39f527895f475c5d906bc2a5
mya413 <lsm940413@gmail.com>
2024-04-10 21:35:15 +0900

    feat: router 적용 및 App 컴포넌트에 GlobalStyle 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
-function App() {
-  return <>Hello, myalog!</>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlobalStyle from "./styles/GlobalStyle";
+import Basic from "./layout/Basic";
+import Readme from "./pages/Readme";
+import Portfolio from "./pages/Portfolio";
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route element={<Basic />}>
+          <Route path="/" element={<Readme />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
